c873c5ba171ae78de62d209f24af9cd9c3a8eb28
Justin Barber <barber.justin@gmail.com>
2015-07-19 10:59:33 -0400

    think i will not end up needing the data pushed, but rather just lean on attributes that are defaulted on elements at init. status: data fetched

--- public/sky_grid.js
+++ public/sky_grid.js
@@ -1,21 +1,37 @@
 SkyGridData = this.sky_grid_data = [];
 
-function DisplayAll(){
+function DisplayGrid(data){
+};
+
+function FetchGridData(el_grid){
+  grid = $(el_grid);
+  url = grid.data('url') + "?start=" + grid.attr('start');
+
+  $.ajax({
+    url: url
+  }).done(DisplayGrid);
+};
+
+function FetchAllGridData(){
+  $.each(SkyGridData, function(index, grid){
+    FetchGridData(grid.e);
+  });
 };
 
 function SkyGrid(){
-  $(this).on("grids.initialized", DisplayAll);
+  $(this).on("grids.initialized", FetchAllGridData);
 
   $('.grid').each( function() {
     grid = $(this);
     columns = grid.data('columns');
+    grid.attr('start', 0);
+    grid.attr('sort', columns[0]);
 
     SkyGridData.push({
       e: this,
       id: grid.attr('id'),
       url: grid.data('url'),
       start: 0,
-      end: 25,
       columns: columns,
       sort: columns[0]
     });
