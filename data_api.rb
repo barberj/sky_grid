817d89fd2b9c174cfef0ceccdeeb1be0682477c5
Justin Barber <barber.justin@gmail.com>
2015-07-18 21:35:13 -0400

    transactions endpoint

---
+++ data_api.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'json'
+require 'faker'
+
+transactions = (1..27).inject([]) do |t, i|
+  record = {
+    id: i,
+    company: Faker::Company.name,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    amount: Faker::Number.decimal(2),
+    description: Faker::Lorem.sentence
+  }
+
+  t << record
+end
+
+get '/transactions' do
+  start = params.fetch('start', 0).to_i
+  queried = transactions[start...start+25]
+
+  {
+    records: queried,
+    count: transactions.size
+  }.to_json
+end
