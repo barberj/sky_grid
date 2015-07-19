95ac7042f387a59382b6224752638fca01e1f6f9
Justin Barber <barber.justin@gmail.com>
2015-07-19 10:03:30 -0400

    syntax error. default and persist sort

--- public/sky_grid.js
+++ public/sky_grid.js
@@ -3,12 +3,14 @@ SkyGridData = this.sky_grid_data = [];
 function SkyGrid(){
 
   grids = $('.grid').each( function() {
+    columns = $(this).data('columns');
 
     SkyGridData.push({
-      url: $(this).data('url');
+      url: $(this).data('url'),
       start: 0,
       end: 25,
-      columns: $(this).data('columns')
+      columns: columns,
+      sort: columns[0]
     });
   });
 }
