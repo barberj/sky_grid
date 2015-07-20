6b106108584d0f215eaa3ab1905575be6e9045a2
Justin Barber <barber.justin@gmail.com>
2015-07-19 21:21:09 -0400

    setup endpoint to handle sorting. loose the symbols since all query params will be strings

--- app.rb
+++ app.rb
@@ -5,13 +5,13 @@ require 'slim'
 
 transactions = (1..27).inject([]) do |t, i|
   record = {
-    id: i,
-    company: Faker::Company.name,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    amount: Faker::Number.decimal(2),
-    description: Faker::Lorem.sentence
+    "id" => i,
+    "company" => Faker::Company.name,
+    "first_name" => Faker::Name.first_name,
+    "last_name" => Faker::Name.last_name,
+    "email" => Faker::Internet.email,
+    "amount" => Faker::Number.decimal(2),
+    "description" => Faker::Lorem.sentence
   }
 
   t << record
@@ -21,7 +21,13 @@ get '/transactions' do
   content_type :json
 
   start = params.fetch('start', 0).to_i
-  queried = transactions[start...start+25]
+  sort_by = params.fetch('sort_by', :id)
+  sort_direction = params.fetch('sort_direction', 'asc')
+
+  sorted = transactions.sort_by{ |t| t[sort_by] }
+  sorted = sorted.reverse if sort_direction == 'desc'
+
+  queried = sorted[start...start+25]
 
   {
     records: queried,
